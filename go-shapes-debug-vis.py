5bbd4bac3f72ed4d7364368799517f56d0276bb5
zhenhuasun <zhenhua.joshua.sun@gmail.com>
2024-10-15 11:42:58 -0400

    Update go-shapes-debug-vis.py

--- go-shapes-debug-vis.py
+++ go-shapes-debug-vis.py
@@ -1,3 +1,7 @@
+import sys
+import subprocess
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'folium'])
 import streamlit as st
 import pandas as pd
 import numpy as np
